ff58bcb1a780a82daeea49bb46ba470117e30128
chetanmulagi <chetanmulagi@gmail.com>
2026-01-07 23:52:01 +0530

    Day 1: basic print statements and variables

---
+++ my-first-code.py
@@ -0,0 +1,19 @@
+print("I am Chetan")
+print("my goal is become python developer")
+print("my target solve real 1000 problems and become expert in python this my goal in 2026 contribute to open source this year 2026")
+print("Mark my words")
+
+Age = 17 # integer
+height = 5.2 # float
+is_student = True # boolean
+name = "Chetan" # string
+print("Age:", Age)
+print("Height:", height)
+print("Is Student:", is_student)
+print("Name:", name)
+# This is a single-line comment
+
+print("This is is my first step towards my goal")
+Date = "7 January 2026"
+print("Date:", Date)
+
