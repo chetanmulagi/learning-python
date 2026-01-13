f3a5cc5fc416ce70ddd3ae6dde7eb447e67e61e3
Chetan mulagi <chetanmulagi@gmail.com>
2026-01-13 10:42:52 -0600

    Create if.py

---
+++ if.py
@@ -0,0 +1,19 @@
+# Print text
+print("Hello World")
+
+# Simple math
+print(2 + 3)
+
+# Variables
+name = "Chetan"
+age = 20
+print(name, age)
+
+# If condition
+x = 5
+if x > 3:
+    print("x is greater than 3")
+
+# Loop
+for i in range(3):
+    print(i)
