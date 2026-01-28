e5e0eb7622f8bad14d32219e04f9907a3f42b0c3
Chetan mulagi <chetanmulagi@gmail.com>
2026-01-28 09:03:40 -0800

    Add files via upload

---
+++ user_input_sum.py
@@ -0,0 +1,11 @@
+a = input("value 1 :") #input value always string when you try to sum its come wrong
+b = input("value 2 :")
+
+sum = (a+b)
+print(sum) # answer is 105
+
+a1 = int(input("value1 :"))
+b1 = int(input("value2 :"))
+
+sum1 = a1 + b1
+print(sum1)
